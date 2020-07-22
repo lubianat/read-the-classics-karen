dfbca1e8ff80d07b819a1d50667e7618ee5c7c8d
lubianat <tiago.lubiana.alves@usp.br>
2020-07-22 09:03:48 -0300

    Automatic update of the EuropePMC list of classics

--- auto-update.sh
+++ auto-update.sh
@@ -1,5 +1,5 @@
 PATH=/home/lubianat/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/lib/pkgconfig:/snap/bin
-cd ~/side/read-the-classics-karen/Europe-PMC-classics
+cd ~/Documents/my_random_stuff/read-the-classics-karen/Europe-PMC-classics
 
 runipy getting_europepemc_classics.ipynb
 
