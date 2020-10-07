9a67da906061caa53d69cb8f62c3c6485dc021c0
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2020-10-07 10:00:08 -0300

    clean repo

--- twitterbot/api_keys.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-oauth = "XFZP8cYm4bNJFS8J4X2R88bwS"
-oauth_secret = "LmKKyRXLGmgGSvyjdWh6HxLQoZwnhrTsFbD04CW0DDtgN14eWg"
-
-token = "1220784098802896897-ao4jypcsjDXCCTbKRNt09DykuZ3v21"
-token_secret = "8wHELL85yTLhNy3fYShaGKXEGTY6qU51sfksBeyImRaOw"
