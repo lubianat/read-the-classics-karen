38335258fe81a3c297442a69b858aab5114b6eb2
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2022-03-05 10:01:31 -0300

    Remove old checkpoints

--- .ipynb_checkpoints/auto-update-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-PATH=/home/lubianat/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/lib/pkgconfig:/snap/bin
-cd ~/side/read-the-classics-karen/Europe-PMC-classics
-
-runipy getting_europepemc_classics.ipynb
-
-cd ..
-git add . 
-git commit -m "Automatic update of the EuropePMC list of classics"
-git push
